744b89c04badcfc516465cac0b308052b5e004d9
Amin Zabardast <amin@192.168.1.135>
2026-03-11 21:20:51 +0200

    feat: Python LSP and Formatter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,12 +5,33 @@ return {
     opts = require "configs.conform",
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
+  -- LSP + Mason wired together in the correct load order:
+  -- mason.nvim → mason-lspconfig.nvim → nvim-lspconfig
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = {},
+      },
+      {
+        "williamboman/mason-lspconfig.nvim",
+        opts = {
+          ensure_installed = { "pyright" },
+        },
+      },
+    },
     config = function()
       require "configs.lspconfig"
     end,
+  },
+
+  -- Auto-installs non-LSP tools (formatters, linters) via Mason
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+      ensure_installed = { "ruff" },
+    },
   },
 
   -- test new blink
